cc8ba2a87b8edd218c28ea1a852faf2b512ea992
Johan van Soest <johan.vansoest@maastro.nl>
2018-08-17 16:06:55 +0200

    Automated the entire process

--- sign.bat
+++ sign.bat
@@ -1,3 +1,11 @@
 del Algorithm.zip
 del Signed.ec
+
+cd site
+Rscript createLibs.r
+
+cd ..\
+
 VarianFileSignerMaastro.exe %~dp0\master %~dp0\site %1
+
+rmdir /S /Q site\libs
--- site/createLibs.r
+++ site/createLibs.r
@@ -2,7 +2,7 @@ packages <- c("jsonlite", "RCurl")
 dir.create("./libs")
 .libPaths(paste0(getwd(), "/libs"))
 
-install.packages(packages)
+install.packages(packages, repos="http://cran.uni-muenster.de/")
 
 devtools::install_bitbucket("jvsoest/sparqlr")
 
